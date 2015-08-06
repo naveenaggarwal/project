ca7626e7f0909ecb8f16101531f6baaa5181b2c2
P V Apoorva <v-pvapoo@microsoft.com>
2015-08-06 04:56:10 +0000

    git-tfs-id: [https://mpsit.visualstudio.com/defaultcollection]$/ScrumProjects/Partner/Portals/RMT;C25373

---
+++ MAIN/Test/RMTTestAutomation/UnitTests/WelcomePage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSCOM.BusinessHelper;
+using MSCOM.Test.TFS;
+using MSCOM.Test.CSV;
+using MSCOM.DDA;
+
+namespace RMT.UnitTests
+{
+    [TestClass]
+    public class WelcomePage
+    {
+        #region Attributes
+        private CSVTestCase currentTC;
+        #endregion
+
+        public TestContext TestContext { get; set; }
+
+        #region Initialization/Finalization
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            //Get TestCaseContext from the CSV file
+            currentTC = new CSVTestCase(string.Format(@"{0}\IterationsData\{1}.csv", MSCOM.Test.Tools.Environment.GetTestContentLocation(), this.TestContext.Properties["TestCaseId"].ToString()));
+            currentTC.ReplaceValuesInAllIterations(MSCOM.Test.Tools.Environment.GetEnvironmentValuesFilePath(MSCOM.Test.Tools.Environment.GetCurrentEnvironment()));
+        }
+
+        #endregion
+
+
+    }
+}
